47845bba9b60f5a77700c974cbe3d9f23981c429
Zakariya Raji <zakariyyaraji@gmail.com>
2024-02-21 11:46:24 +0100

    setup middlewares, validations, utilities, auth and plugins, connecting to MongoDB,

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-const express = require("express");
-const app = express();
+import Mongoose from "mongoose";
+import app from "./app.js";
+import Config from "./config/config.js";
+import Logger from "./config/logger.js";
 
-app.get('/api/hello-user', (req, res) => {
-    const user = req.query.user;
-    res.json('Howdy ' + user + ', First Milestone!');
+let server;
+Mongoose.connect(Config.mongoose.url, Config.mongoose.options).then(() => {
+  Logger.info("Connected to MongoDB");
+  server = app.listen(Config.port, () => {
+    Logger.info(`uRide Server running on port: ${Config.port}
+      -----------------------------------------
+      Running on ${process.env.NODE_ENV}
+      -----------------------------------------
+    `);
+    Logger.info(`SMPT PORT ${Config.email.smtp.host}`);
   });
+});
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      Logger.info("SIMS Server closed, Bye Bye my friends!");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  Logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  Logger.info("SIGTERM received");
+  if (server) {
+    server.close();
+  }
 });
