0c454b68bfa03f3af6d632f6cbb4dcc1464973e9
ZAKARIYA RAJI <zakariyyaraji@gmail.com>
2024-04-29 22:05:51 +0100

    pm2 config update

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	apps: [
+		{
+			name: "uRide_Backend",
+			script: "./src/index.js",
+			exp_backoff_restart_delay: 100,
+			max_memory_restart: "1G",
+			max_restarts: 10,
+			min_uptime: 2000,
+		},
+	],
+};
