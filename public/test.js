22db2075369b04a8967beac03b07ef3fe8a01f5c
Zakary <zakariyyaraji@gmail.com>
2024-06-22 14:51:34 +0100

    Update test.js

--- public/test.js
+++ public/test.js
@@ -2,9 +2,9 @@ const app = require("./app")
 const supertest = require("supertest")
 const request = supertest(app)
 describe("Testing root", () => {
-    it("should return a response", async () => {
+    it("should be equal", async () => {
         const response = await request.get("/")
-        expect(response.status).toBe(200)
+        expect(2 + 2).toBe(4)
         // expect(response.text).toBe("Hello there!");
     })
 })
