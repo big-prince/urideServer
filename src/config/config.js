57d0cb9fa387a663f279efe11f9641db1702b0e4
ZAKARIYA RAJI <zakariyyaraji@gmail.com>
2024-06-26 15:55:38 +0100

    log-rotation and download

--- src/config/config.js
+++ src/config/config.js
@@ -4,20 +4,35 @@ import Joi from 'joi';
 dotenv.config();
 
 const envVarsSchema = Joi.object()
-  .keys({
-    // NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('Mongo DB url'),
-    JWT_SECRET: Joi.string().required().description('JWT secret key'),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
-    SMTP_USERNAME: Joi.string().description('username for email server'),
-    SMTP_PASSWORD: Joi.string().description('password for email server'),
-    EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
-  })
-  .unknown();
+	.keys({
+		// NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+		PORT: Joi.number().default(3000),
+		MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+		JWT_SECRET: Joi.string().required().description("JWT secret key"),
+		JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
+			.default(30)
+			.description("minutes after which access tokens expire"),
+		JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
+			.default(30)
+			.description("days after which refresh tokens expire"),
+		SMTP_HOST: Joi.string().description(
+			"server that will send the emails"
+		),
+		SMTP_PORT: Joi.number().description(
+			"port to connect to the email server"
+		),
+		SMTP_USERNAME: Joi.string().description(
+			"username for email server"
+		),
+		SMTP_PASSWORD: Joi.string().description(
+			"password for email server"
+		),
+		EMAIL_FROM: Joi.string().description(
+			"the from field in the emails sent by the app"
+		),
+		API_KEY: Joi.string().description("API KEY is needed"),
+	})
+	.unknown();
 
 const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
@@ -26,32 +41,33 @@ if (error) {
 }
 
 export default {
-  // env: envVars.NODE_ENV,
-  port: envVars.PORT,
-  mongoose: {
-    // url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
-    url: envVars.MONGODB_URL,
-    options: {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  },
-  jwt: {
-    secret: envVars.JWT_SECRET,
-    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
-    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
-    resetPasswordExpirationMinutes: 10,
-  },
-  email: {
-    smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
-      auth: {
-        user: envVars.SMTP_USERNAME,
-        pass: envVars.SMTP_PASSWORD,
-      },
-    },
-    from: envVars.EMAIL_FROM,
-  },
+	// env: envVars.NODE_ENV,
+	port: envVars.PORT,
+	mongoose: {
+		// url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+		url: envVars.MONGODB_URL,
+		options: {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+	},
+	jwt: {
+		secret: envVars.JWT_SECRET,
+		accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+		refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+		resetPasswordExpirationMinutes: 10,
+	},
+	email: {
+		smtp: {
+			host: envVars.SMTP_HOST,
+			port: envVars.SMTP_PORT,
+			auth: {
+				user: envVars.SMTP_USERNAME,
+				pass: envVars.SMTP_PASSWORD,
+			},
+		},
+		from: envVars.EMAIL_FROM,
+	},
+	API_KEY: "iamthereasonwhyitsaveryimportantsecret", 
 };
