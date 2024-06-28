f3b3fb94e5010b4573f338b256aef101e22ced6b
Prince <ograindy@gmail.com>
2024-06-28 18:21:43 +0100

    Prepared review System

---
+++ src/modules/reviews/review.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import auth from "../../middlewares/auth.js";
+import validate from "../../middlewares/validate.js";
+import reviewController from "../reviews/review.controller.js";
+
+const router = Router();
+
+export default Router;
