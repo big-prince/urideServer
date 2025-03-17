a9ca0123f037d04bedd64576b7c8b24b26e63c4c
Prince <ograindy@gmail.com>
2025-03-17 19:46:35 +0100

    Swtiched back to nodemailer for render deployment for development

--- src/modules/com/emails/email.service.js
+++ src/modules/com/emails/email.service.js
@@ -1,4 +1,4 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 import { welcomeEmail } from "./templs/welcome.js";
 import { passwordResetEmail } from "./templs/reset.js";
 import { sendOtpEmailTemplate } from "./templs/otp.js";
@@ -7,46 +7,39 @@ import User from "../../users/user.model.js";
 import httpStatus from "http-status";
 import ApiError from "../../../utils/ApiError.js";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-async function fireEmail(message) {
-  console.log("Starting fireEmail with Resend...");
-  try {
-    console.log("Attempting to send email with Resend...");
-    const { data, error } = await resend.emails.send(message);
-    if (error) {
-      console.error("Failed to send email with Resend:", error);
-      return {
-        messageId: null,
-        status: "failed",
-        message: "Failed to send email with Resend",
-      };
-    }
-    console.log("Email sent successfully:", data);
-    return {
-      messageId: data.id, // Resend’s unique ID for the email
-      status: "sent",
-    };
-  } catch (error) {
-    console.error("Failed to send email with Resend:", error);
-    return {
-      messageId: null,
-      status: "failed",
-      message: "Failed to send email with Resend",
-    };
-  }
-}
-
-export const sendWelcomeEmail = async (receiverEmail, receiverName) => {
-  const message = {
-    from: process.env.EMAIL_FROM,
-    to: receiverEmail,
-    subject: "Welcome to uRide!",
-    html: welcomeEmail(receiverName),
-  };
-  return fireEmail(message);
+let config = {
+  service: "gmail", // your email domain
+  auth: {
+    user: process.env.SMTP_USERNAME, // your email address
+    pass: process.env.SMTP_PASSWORD, // your password
+  },
 };
 
+let transporter = nodemailer.createTransport(config);
+
+//send welcome email
+export const sendWelcomeEmail = async (receiverEmail, receiverName) => {
+  let message = {
+    from: process.env.EMAIL_FROM, // sender address
+    to: receiverEmail, // list of receivers
+    subject: "Welcome to uRide!", // Subject line
+    html: welcomeEmail(receiverName), // html body
+  };
+
+  try {
+    const info = await fireEmail(message);
+    console.log(info); // Add this line to log the info object
+    return {
+      msg: "Email sent",
+      info: info.messageId,
+      preview: nodemailer.getTestMessageUrl(info),
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+// send forgot password email
 export const sendForgotPasswordEmail = async (
   receiverEmail,
   resetPasswordToken
@@ -58,24 +51,47 @@ export const sendForgotPasswordEmail = async (
     subject: "Reset Your Password",
     html: passwordResetEmail(receiverEmail, resetPasswordLink),
   };
-  return fireEmail(message);
+
+  try {
+    const info = await fireEmail(message);
+    console.log(info); // Add this line to log the info object
+    return {
+      msg: "Email sent",
+      info: info.messageId,
+      preview: nodemailer.getTestMessageUrl(info),
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
+// send otp to email
 export const sendOTPEmail = async (receiverEmail, otp) => {
   const user = await User.findOne({ email: receiverEmail });
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, "User not Found");
   }
-  console.log(process.env.EMAIL_FROM);
   const message = {
     from: process.env.EMAIL_FROM,
     to: receiverEmail,
     subject: "OTP from uRide",
     html: sendOtpEmailTemplate(user.firstName, otp),
   };
-  return fireEmail(message);
+
+  try {
+    const info = await fireEmail(message);
+    // console.log(info); // Add this line to log the info object
+    return {
+      msg: "Email sent",
+      info: info.messageId,
+      preview: nodemailer.getTestMessageUrl(info),
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
+//send security code Email
 export const sendSecurityCodeEmail = async (
   receiverEmail,
   code,
@@ -84,7 +100,9 @@ export const sendSecurityCodeEmail = async (
   console.log(rideDetails, "The Details");
   const user = await User.findOne({ email: receiverEmail });
   const driverDetails = await User.findOne({ email: rideDetails.driver });
+  //construct driver fullname
   const driverFullName = `${driverDetails.firstName} ${driverDetails.lastName}`;
+
   const rideOptions = {
     driver: driverFullName,
     departure_time: rideDetails.departure_time,
@@ -92,11 +110,33 @@ export const sendSecurityCodeEmail = async (
     origin: rideDetails.origin,
     price: rideDetails.price,
   };
+  console.log(rideOptions);
   const message = {
     from: process.env.EMAIL_FROM,
     to: receiverEmail,
     subject: "Security Code from uRide",
     html: sendSecurityCodeEmailTemplate(user.firstName, code, rideOptions),
   };
-  return fireEmail(message);
+
+  try {
+    const info = await fireEmail(message);
+    console.log(info);
+    return {
+      msg: "Email sent",
+      info: info.messageId,
+      preview: nodemailer.getTestMessageUrl(info),
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 };
+
+async function fireEmail(message) {
+  try {
+    const info = await transporter.sendMail(message);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error(error);
+  }
+}
