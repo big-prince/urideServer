c7869a52b43a89d661eead3b860513e6943d0abd
Michael Seth <michaelseth78@gmail.com>
2025-03-20 10:02:02 +0100

    init commit

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const envVarsSchema = Joi.object()
   .keys({
+    // NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
     JWT_SECRET: Joi.string().required().description("JWT secret key"),
@@ -32,6 +33,8 @@ const { value: envVars, error } = envVarsSchema
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
+
+export const MONGODB_URL = envVars.MONGODB_URL;
 
 export default {
   // env: envVars.NODE_ENV,
