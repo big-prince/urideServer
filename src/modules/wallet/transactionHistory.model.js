376d85d59d4c451cd7dc7f2fa12e5f29a08af8c8
Prince <ograindy@gmail.com>
2025-04-28 09:08:58 +0100

    FIXED:   src/modules/wallet/transactionHistory.model.test.js

--- src/modules/wallet/transactionHistory.model.js
+++ src/modules/wallet/transactionHistory.model.js
@@ -1,3 +1,6 @@
+import mongoose from "mongoose";
+import tType from "../../config/transaction.type.js";
+
 const transactionHistorySchema = new mongoose.Schema(
   {
     userId: {
@@ -16,7 +19,7 @@ const transactionHistorySchema = new mongoose.Schema(
         type: String,
         required: true,
         enum: ["top-up", "payment", "order"],
-        default: "top-up"
+        default: "payment"
       },
       orderId: { type: String, ref: "Order", default: null },
     },
@@ -29,3 +32,30 @@ const transactionHistorySchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+transactionHistorySchema.index({ "data.reference": 1 }, { unique: true });
+
+// Function to drop all indexes for Order
+// Function to drop all indexes for TransactionHistory
+const dropAllIndexes = async () => {
+  try {
+    if (mongoose.connection.collections.transactionhistories) {
+      console.log('Current indexes for TransactionHistory collection:');
+      const indexes = await mongoose.connection.collections.transactionhistories.listIndexes().toArray();
+      console.log(indexes);
+      await mongoose.connection.collections.transactionhistories.dropIndexes();
+      console.log('All indexes for TransactionHistory collection have been dropped');
+    } else {
+      console.log('TransactionHistory collection does not exist yet');
+    }
+  } catch (error) {
+    console.error('Error dropping indexes:', error);
+  }
+}
+mongoose.connection.once('open', dropAllIndexes);
+
+const TransactionHistory = mongoose.model(
+  "TransactionHistory",
+  transactionHistorySchema
+);
+
+export default TransactionHistory;
